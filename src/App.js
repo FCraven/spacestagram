d4f13eaa57cdc4492be46205ecdfdbc9844db762
FCraven <craven.francis@gmail.com>
2022-01-17 14:25:05 -0500

    initial commit with to-dos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  //How do I build this app?
+
+// Fetch data from one of NASA’s APIs and display the resulting images
+  // Search results should come from https://api.nasa.gov -
+  // We’ve provided screenshots below of demo apps we built using the
+    // Astronomy Picture of the Day
+    // Mars Rover Photos APIs (along with Shopify’s open source React component library: Polaris).
+
+  // You are free to use any NASA API you like
+
+//EACH CARD
+
+// Display descriptive data for each image:
+// Each image result should list:
+  // a title
+  // date of capture (ideally in earth_date)
+  // and a button to “like” that image.
+    // Each image can be “liked”
+    // able to undo their “like”
+
+// The HTML that ends up being served client-side should be accessible and semantic (MDN reference)
+
+
+// Extras
+
+  // Save likes if the user leaves or reloads the page
+
+  // Animate the “like” action (might we suggest a heart?)
+
+  // Add a loading state while we wait for NASA’s API to return data
+
+  // Create shareable links for each image
+
+  // Add a date-picker to be able to browse photos starting from a specific date
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
